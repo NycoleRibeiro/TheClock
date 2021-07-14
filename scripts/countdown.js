42ea3611bd409806f6bba202474b87400c216f0f
Nycole Ribeiro <rpnyck@gmail.com>
2021-07-13 21:30:07 -0300

    add close and show modal

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,9 +1,19 @@
-function showModal(modalID) {
-    const modal = document.getElementById(modalID);
-    modal.classList.add('mostrar');
-}
+// ta funcionando agora esta poha que por algum motivo não funciona sem o window.onload que eu nem sei pra q serve, é isso
 
-const logo = document.querySelector("#config");
-logo.addEventListener('click', function() {
-    showModal('modal-settings');
-});
+window.onload=function(){
+    let configButton = document.querySelector(".config");
+    let modal = document.querySelector('#modal-settings');
+
+    configButton.addEventListener('click', () => {
+        modal.classList.toggle('mostrar');
+        modal.addEventListener('click', (e) => {
+            //console.log(e.target) //para saber onde ta clicando
+            if(e.target.id == 'modal-settings' || e.target.className == 'closeButton') {
+                modal.classList.remove('mostrar') // ao clicar fora ou no x do modal ele fecha
+            };
+            if(e.target.className == 'button') {
+                modal.classList.remove('mostrar') // por enquanto ta só fechando, mas ainda tem q salvar os input
+            };
+        })
+    })
+}
