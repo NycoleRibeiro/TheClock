693a319617b8e3317af27433522d06538a2412fb
Nycole Ribeiro <rpnyck@gmail.com>
2021-08-04 18:46:48 -0300

    Pomodoro Timer Progress

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -10,9 +10,21 @@ window.onload=function(){
             if(e.target.id == 'modal-settings' || e.target.className == 'closeButton') {
                 modal.classList.remove('mostrar') // ao clicar fora ou no x do modal ele fecha
             };
+            /*VALIDAÇÃO*/
             if(e.target.className == 'button') {
-                modal.classList.remove('mostrar')
-                start();
+                // pegando o valor
+                let tempoSolicitado = document.querySelector(".focustimeInput").value;
+                let min = tempoSolicitado.slice(0,2);
+                let seg = tempoSolicitado.slice(3,5);
+                // verificar espaços usados
+                let quantia = tempoSolicitado.length
+                if (quantia != 5) {
+                    alert("Use o padrão: 00:00");
+                } else if (isNaN(min) || isNaN(seg)) {
+                    alert("Use apenas números no padrão 00:00");
+                } else {
+                    modal.classList.remove('mostrar');
+                }
             };
         })
     })
@@ -30,72 +42,94 @@ window.onload=function(){
             };
         })
     })
-}
 
-function iniciar (){ 
-    // Pegando o tempo de foco
-    let focusTime = document.querySelector(".focustimeInput").value;
-    let horaFoco = parseInt(focusTime.slice(0,2));
-    let minFoco = parseInt(focusTime.slice(3,5));
-    let segFoco = parseInt(focusTime.slice(6,8));
-    // Pegando tempo de pausa
-    let restTime = document.querySelector(".stoptimeInput").value;
-    let horaPause = parseInt(restTime.slice(0,2));
-    let minPause = parseInt(restTime.slice(3,5));
-    let segPause = parseInt(restTime.slice(6,8));
-    // Pegando quantidade de sessões
-    let session = document.querySelector(".sessionsInput").value;
-    // Texto da qtd de sessions
-    let textSession = document.querySelector(".sessions_number");
+    // FUNÇÕES DO TIMER DO POMODORO
+    let start = document.getElementById('start');
+    let stop = document.getElementById('stop');
+    let reset = document.getElementById('reset');
 
-    for (var i = parseInt(session); i > 0; i--) {
-        textSession.innerHTML = i;
-        console.log(i);
-    }
-}
+    //Pegando o tempo de foco
+    let tempoFoco = document.querySelector(".focustimeInput").value;
+    let minFoco = tempoFoco.slice(0,2);
+    let segFoco = tempoFoco.slice(3,5);
 
-function start() {
-    // Carregando sons
-    var click = new Audio('sounds/click.mp3');
-    var bell = new Audio('sounds/bell.mp3');
-    click.play();
+    let focusMin = document.getElementById('focus_minutes');
+    let focusSec = document.getElementById('focus_seconds');
+    focusMin.innerHTML = minFoco;
+    focusSec.innerHTML = segFoco;
 
-    hours = 1;
-    minutes = 24;
-    seconds = 59;
+    //Pegando o tempo de pausa
+    let tempoPausa = document.querySelector(".stoptimeInput").value;
+    let minPausa = tempoPausa.slice(0,2);
+    let segPausa = tempoPausa.slice(3,5);
 
-    const segundos = document.getElementById('segundos');
-    const minutos = document.getElementById('minutos');
-    const horas = document.getElementById('horas');
-    segundos.innerHTML = seconds;
-    minutos.innerHTML = minutes;
-    horas.innerHTML = hours;
+    let PauseMin = document.getElementById('pause_minutes');
+    let PauseSec = document.getElementById('pause_seconds');
+    PauseMin.innerHTML = minPausa;
+    PauseSec.innerHTML = segPausa;
 
-    var hours_interval = setInterval(hoursTimer, 3600000);
-    var minutes_interval = setInterval(minutesTimer, 60000);
-    var seconds_interval = setInterval(secondsTimer, 1000);
+    let startTimer;
 
-    const formatarDigito = (digito) => `0${digito}`.slice(-2);
-
-    function secondsTimer() {
-        seconds--;
-        segundos.innerHTML = formatarDigito(seconds);
-        if (seconds <= 0) {
-            minutes--;
-            seconds = 59;
-            if (minutes <= 0) {
-                hours--;
-                minutes = 59;
-                if (hours <= 0) {
-                    clearInterval(hours_interval);
-                    clearInterval(minutes_interval);
-                    clearInterval(seconds_interval);
-
-                    document.querySelector('.frase').innerHTML = "Hora da Pausa";
-                }
-            }
+    start.addEventListener('click', function(){
+        if(startTimer === undefined){
+            startTimer = setInterval(timer, 1000)
         } else {
-            segundos.innerHTML = seconds;
+            alert("O contador já está rodando!");
+        }
+    })
+
+    reset.addEventListener('click', function(){
+        focusMin.innerText = "00";
+        focusSec.innerText = "00";
+
+        PauseMin.innerText = "00";
+        PauseSec.innerText = "00";
+
+        document.getElementById('counter').innerText = 0;
+        stopInterval()
+        startTimer = undefined;
+    })
+
+    stop.addEventListener('click', function(){
+        stopInterval()
+        startTimer = undefined;
+    })
+
+
+    //Start Timer Function
+    function timer(){
+        //Work Timer Countdown
+        if(focusSec.innerText != 0){
+            focusSec.innerText--;
+        } else if(focusMin.innerText != 0 && focusSec.innerText == 0){
+            focusSec.innerText = 59;
+            focusMin.innerText--;
+        }
+
+        //Break Timer Countdown
+        if(focusMin.innerText == 0 && focusSec.innerText == 0){
+            if(PauseSec.innerText != 0){
+                PauseSec.innerText--;
+            } else if(PauseMin.innerText != 0 && PauseSec.innerText == 0){
+                PauseSec.innerText = 59;
+                PauseMin.innerText--;
+            }
+        }
+
+        //Increment Counter by one if one full cycle is completed
+        if(focusMin.innerText == 0 && focusSec.innerText == 0 && PauseMin.innerText == 0 && PauseSec.innerText == 0){
+            focusMin.innerText = 25;
+            focusSec.innerText = "00";
+
+            PauseMin.innerText = 5;
+            PauseSec.innerText = "00";
+
+            document.getElementById('counter').innerText++;
         }
     }
+
+        //Stop Timer Function
+        function stopInterval(){
+            clearInterval(startTimer);
+        }
 }
